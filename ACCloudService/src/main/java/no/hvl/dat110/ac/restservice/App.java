0ce5e4bbaeb79feef3ceb7cd0f3fbc305f58b53c
585007 <585007@stud.hvl.no>
2021-05-04 17:53:50 +0200

    app ferdig, men ikke testet

--- ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/App.java
+++ ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/App.java
@@ -46,6 +46,58 @@ public class App {
 		// TODO: implement the routes required for the access control service
 		// as per the HTTP/REST operations describined in the project description
 		
+		//receive json message, status
+		//return json AccessEntry
+		post("/accessdevice/log/", (req, res) ->{
+			Gson gson = new Gson();
+			AccessMessage msg = gson.fromJson(req.body(), AccessMessage.class);
+			
+			int id = accesslog.add(msg.getMessage());
+			
+			return gson.toJson(accesslog.get(id));
+		});
+		
+		//return json log of all entries
+		get("/accessdevice/log/", (req, res) ->{
+			
+			return accesslog.toJson();
+		});
+		
+		//return single result from log
+		get("/accessdevice/log/{id}", (req, res) ->{
+			Gson gson = new Gson();
+			
+			int id = req.attribute("id");
+			
+			return gson.toJson(accesslog.get(id));
+		});
+		
+		//Updating the access code
+		//returning json of new code
+		put("/accessdevice/code", (req, res) ->{
+			Gson gson = new Gson();
+			AccessCode newCode = gson.fromJson(req.body(), AccessCode.class);
+			
+			accesscode.setAccesscode(newCode.getAccesscode());
+			
+			return gson.toJson(accesscode);
+		});
+		
+		//returning curent code in json
+		get("/accessdevice/code", (req, res) ->{
+			Gson gson = new Gson();
+			
+			return gson.toJson(accesscode);
+		});
+		
+		//deletes all entries in log
+		//return the empty access log
+		delete("/accessdevice/log/", (req, res) ->{
+			accesslog.clear();
+			
+			return accesslog.toJson();
+		});
+		
     }
     
 }
